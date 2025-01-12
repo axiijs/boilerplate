ff102e3879d6fab492ff193ae88ce753ab9f2547
sskyy <skyking_H@hotmail.com>
2025-01-12 22:10:57 +0800

    initial commit

---
+++ index.tsx
@@ -0,0 +1,4 @@
+import {createRoot, createElement} from "axii";
+import { App } from './src/App'
+
+createRoot(document.getElementById('root')!).render(<App/>)
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import {RenderContext} from "axii";
+
+export function App({}, {createElement}: RenderContext)  {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
